40eb255222941310ff0c651086b4d1933d374a2e
mukilvarma <33450604+mukilvarma@users.noreply.github.com>
2024-10-20 18:50:27 +0530

    Delete src directory

--- src/main/java/com/mukil/mealrecommendation/interfaces/NutritionAnalysisService.java
+++
@@ -1,8 +0,0 @@
-package com.mukil.mealrecommendation.interfaces;
-
-import com.mukil.mealrecommendation.model.MenuItem;
-
-public interface NutritionAnalysisService {
-    MenuItem analyzeNutrition(MenuItem menuItem) throws Exception;
-}
-
