1345481c53d441cfecc53a18e67b083e17f44f53
mukilvarma <33450604+mukilvarma@users.noreply.github.com>
2024-10-22 15:49:59 +0530

    Update SecurityConfig.java

--- src/main/java/com/mukil/mealrecommendation/config/SecurityConfig.java
+++ src/main/java/com/mukil/mealrecommendation/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Your frontend URL
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://13.203.42.185")); // frontend URLs
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Allow credentials if necessary
